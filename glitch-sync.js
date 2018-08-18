c99dd10cfa9ff631233380e3c8999c5c82c66d40
hat <jburns@chapman.edu>
2018-08-18 13:07:38 -0700

    testing sync

---
+++ glitch-sync.js
@@ -0,0 +1,3 @@
+var nrc = require('node-run-cmd');
+
+nrc.run("GLITCH_PROJECT_ID='65cbe78c-c018-45cf-a47b-ef157907457c' GLITCH_TOKEN='be54db4a-00e7-408e-8b93-f173602590f4' GH_REPO='jhburns/val-bot' DEBUG=sync-glitch* ./node_modules/.bin/sync-glitch./node_modules/.bin/sync-glitch");
